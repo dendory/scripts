d1a76333e5dcb0837b33cf7d689f5b7f29ddabe7
Patrick Lambert <dendory@live.ca>
2016-04-02 14:47:25 -0400

    Create aws_destroy_instance.ps1

---
+++ aws_destroy_instance.ps1
@@ -0,0 +1,42 @@
+#
+# This script will terminate an EC2 instance based on the configuration passed to it.
+# Made by Patrick Lambert released under the MIT license.
+#
+# -InstanceId The EC2 instance id to terminate
+# -Hostname The hostname to remove for this host
+# -ZoneId The Route53 zone ID on which to remove the hostname
+#
+param([Parameter(Mandatory=$true)][string]$InstanceId, [Parameter(Mandatory=$true)][string]$Hostname, [Parameter(Mandatory=$true)][string]$ZoneId)
+$ErrorActionPreference = "Stop"
+
+# Fetch the EBS drive id and IP address
+$result = Get-EC2Instance -Filter @{Name = "instance-id"; Value = $InstanceId} |select * -ExpandProperty Instances
+[string]$ip = $result.PublicIpAddress
+$volumes = Get-EC2Volume -Filter @{ Name = "attachment.instance-id"; Value = $InstanceId }
+
+# Terminate the instance
+Write-Host "Terminating instance $Instance..."
+Stop-EC2Instance $InstanceId -Force -Terminate
+
+# Remove the EBS
+foreach($volume in $volumes)
+{
+    [string]$volid = $volume.VolumeId
+    Write-Host "Removing EBS volume $volid..."
+    Remove-EC2Volume -VolumeId $volid -Force
+}
+
+# Remove A record
+Write-Host "Removing A record..."
+$change = New-Object Amazon.Route53.Model.Change
+$change.Action = "DELETE"
+$change.ResourceRecordSet = New-Object Amazon.Route53.Model.ResourceRecordSet
+$change.ResourceRecordSet.Name = $Hostname
+$change.ResourceRecordSet.Type = "A"
+$change.ResourceRecordSet.TTL = 600
+$change.ResourceRecordSet.ResourceRecords.Add(@{Value=$ip})
+
+Edit-R53ResourceRecordSet -HostedZoneId $ZoneId -ChangeBatch_Change $change
+
+# Done
+Write-Host "Done."
