e3dc493bfcff9762318a1b578c87fbbcd93da24f
Patrick Lambert <dendory@gmail.com>
2017-06-10 13:57:31 +0000

    smalel changes

--- centos-httpd-ssl-remove.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#
-# Remove SSL support and LetsEncrypt
-#
-
-yum -y remove python-certbot-apache certbot mod_ssl
-mv /etc/httpd/conf/httpd.default /etc/httpd/conf/httpd.conf
-systemctl restart httpd
